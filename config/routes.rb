24c3ed50bcce9121c3a813615f01fc42fd74201b
Tomohiro Yamaguchi <tomo1301nbabaske@gmail.com>
2020-07-06 21:50:55 +0900

    chat-space グループ機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
-# ルーティングを追加
 Rails.application.routes.draw do
   devise_for :users
-  # 下の行は削除する
-  # get 'messages/index
-
-  root "messages#index"
+  root 'groups#index'
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
 end
